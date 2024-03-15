13d5c742802f8f7d70c5deaa089c565744bc3df4
Thiago Epiphanio <142552119+ThiagoEpiphanio@users.noreply.github.com>
2024-03-14 21:36:05 -0300

    Aula 1403

---
+++ AulaLP1403/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Página Inicial</title>
+</head>
+<body>
+    
+    <h1>Aula 1403</h1>
+
+    <a href="combustivel.php"> Primeiro exemplo</a><br>
+    <a href=""> Segundo exemplo</a><br>
+    <a href=""> Terceiro exemplo</a><br>
+
+</body>
+</html>
